e8433865d5dcf374f7c20df02c730a5de06cabe7
Thayana Vilaça Fortunato <athay.thayana@gmail.com>
2023-02-27 14:42:12 -0300

    commit 2

---
+++ src/NotasEstudante.java
@@ -0,0 +1,13 @@
+//INDENTAÇÃO
+public class NotasEstudante {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO"); //INDENTADO EM CADA BLOCO, DEIXA MAIS LEGIVÉL//
+        else if (mediaFinal == 6)
+           System.out.println("PROVA DECISIVA");
+        else
+            System.out.println("APROVADA");
+
+        }
+    }
