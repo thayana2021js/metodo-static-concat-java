d7617dcd5caa50e8d515c767b153c1e3787eff4e
Thayana Vilaça Fortunato <athay.thayana@gmail.com>
2023-02-27 13:10:13 -0300

    first commit

---
+++ anatomia1/Java-anatomia-CLASS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Java_anatomia_CLASS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/MinhaClasse.java
@@ -0,0 +1,38 @@
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		String primeiroNome = "Thayana";
+		String segundoNome = "Vilaca Fortunato";
+
+
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto);
+
+	}
+
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
